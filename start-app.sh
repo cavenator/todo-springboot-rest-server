21ee59e6d9053e4bebd0d10c3f903c3b4470b703
acave <acave@cj.com>
2016-07-30 10:13:43 -0700

    Updated application to export ip address from docker machine and include it in docker run command and datasource url in application.properties

---
+++ start-app.sh
@@ -0,0 +1,2 @@
+source ./start-todo-db.sh
+mvn package && java -jar target/todo-app-springboot-0.1-SNAPSHOT.jar
--- start-todo-db.sh
+++ start-todo-db.sh
@@ -7,8 +7,9 @@ if [ $count -eq 1 ]
         echo "Note to self .... create data volume for this container and learn how to back it up in case of container failure"
    else
        echo "okay .... Lets make a new one!"
-       # the host mapping is REALLY the host of the docker machine that houses the postgres db. Find a way to map the docker machine ip to localhost.
-       docker run -d --name=todo-db -p 192.168.99.100:5432:5432 postgres:9.1.18
+       #extract the ip address from the docker host for port forwarding (necessary if wanting to connect to container from within docker-machine)
+       export DOCKER_IP=$(echo $DOCKER_HOST | egrep -o '[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}')
+       docker run -d --name=todo-db -p $DOCKER_IP:5432:5432 postgres:9.1.18
        echo "Docker container running postgres db up and going .... going to wait 3 seconds before setting up db"
        sleep 3
 fi
