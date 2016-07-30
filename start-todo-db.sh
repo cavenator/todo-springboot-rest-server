f1ab69c2b7797328215b39f25c240553f2d06a0e
acave <acave@cj.com>
2016-07-30 12:28:48 -0700

    Replaced TomCat with Jetty. Return an error if DOCKER_IP cannot be extracted

--- start-todo-db.sh
+++ start-todo-db.sh
@@ -1,5 +1,12 @@
 count=$(docker ps -a | grep todo-db | wc -l)
-echo $count
+export DOCKER_IP=$(echo $DOCKER_HOST | egrep -o '[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}')
+
+if [ -z $DOCKER_IP ]
+    then
+	echo 'ERROR:  DOCKER_IP is not found!  Be sure that you could communicate with the docker-daemon first'
+	exit 1
+fi
+
 if [ $count -eq 1 ]
    then
         echo "Starting todo-db for app"
@@ -8,7 +15,6 @@ if [ $count -eq 1 ]
    else
        echo "okay .... Lets make a new one!"
        #extract the ip address from the docker host for port forwarding (necessary if wanting to connect to container from within docker-machine)
-       export DOCKER_IP=$(echo $DOCKER_HOST | egrep -o '[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}')
        docker run -d --name=todo-db -p $DOCKER_IP:5432:5432 postgres:9.1.18
        echo "Docker container running postgres db up and going .... going to wait 3 seconds before setting up db"
        sleep 3
